34d55fe4fee11818676f0f04230717387c8e6b24
Kat Koler <katjusa.koler@gmail.com>
2019-11-06 18:21:01 +0000

    add variable number of parts to estimates03

--- src/mean_pcor2_barcode_hallmark_estimates03.R
+++ src/mean_pcor2_barcode_hallmark_estimates03.R
@@ -9,11 +9,20 @@ cmd_args <- commandArgs(trailingOnly = T)
 geneset_file <- cmd_args[1]
 output_folder <- cmd_args[2]
 
+# ==== Pathway Annotation ====
+# Filtered gene set annotation
+gs_lst = readRDS(paste("../data/",geneset_file, sep=""))
+
+
+# indices for pathway pairs 
+number_of_pathways = choose(length(gs_lst),2)
+# split pathway pairs in chunks
+pairs_chunks <- split(1:number_of_pathways, ceiling(1:number_of_pathways/1000))
 
 # empty variable to store results
 pcxn = c()
 pb = txtProgressBar(min=0,max=2,initial=0,style=3)
-for(k in 1:1){
+for(k in 1:length(pairs_chunks)){
   pcxn = rbind(pcxn, readRDS(paste0("../",output_folder,"/mean_pcor2_barcode/res/pcxn_mean_pcor2_barcode_part",k,".RDS")))
   setTxtProgressBar(pb,k)
 }
